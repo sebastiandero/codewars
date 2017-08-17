4b6047d577a52130e1304a59b8b3b5e5e062ac15
Sebastian De Ro <sebastian.dero@yahoo.de>
2017-08-17 22:09:23 +0200

    camelcase

---
+++ test/com/sebastiandero/CamelCaseTest.java
@@ -0,0 +1,20 @@
+package com.sebastiandero;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CamelCaseTest {
+    @Test
+    public void testSomeUnderscoreLowerStart() {
+        String input = "the_Stealth_Warrior";
+        System.out.println("input: "+input);
+        assertEquals("theStealthWarrior", CamelCase.toCamelCase(input));
+    }
+    @Test
+    public void testSomeDashLowerStart() {
+        String input = "the-Stealth-Warrior";
+        System.out.println("input: "+input);
+        assertEquals("theStealthWarrior", CamelCase.toCamelCase(input));
+    }
+}
