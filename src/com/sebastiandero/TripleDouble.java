a09120acf824c74a0467a1979f2447a6c696be81
Sebastian De Ro <sebastian.dero@yahoo.de>
2017-08-17 21:28:11 +0200

    tripledouble

---
+++ src/com/sebastiandero/TripleDouble.java
@@ -0,0 +1,26 @@
+package com.sebastiandero;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TripleDouble {
+
+    public static int TripleDouble(long num1, long num2) {
+        Pattern pattern1 = Pattern.compile("(000|111|222|333|444|555|666|777|888|999)");
+        Pattern pattern2 = Pattern.compile("(00|11|22|33|44|55|66|77|88|99)");
+        Matcher matcher1 = pattern1.matcher(String.valueOf(num1));
+        Matcher matcher2 = pattern2.matcher(String.valueOf(num2));
+
+        while (matcher1.find()) {
+            String num1Match = matcher1.group(1);
+            while (matcher2.find()) {
+                String num2Match = matcher2.group(1);
+                if (num1Match.charAt(0) == num2Match.charAt(0)) {
+                    return 1;
+                }
+            }
+        }
+
+        return 0;
+    }
+}
---
+++ test/com/sebastiandero/TripleDoubleTest.java
@@ -0,0 +1,28 @@
+package com.sebastiandero;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TripleDoubleTest {
+
+    @Test
+    public void test1() {
+        assertEquals(1, TripleDouble.TripleDouble(451999277L, 41177722899L));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(0, TripleDouble.TripleDouble(1222345L, 12345L));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals(0, TripleDouble.TripleDouble(12345L, 12345L));
+    }
+
+    @Test
+    public void test4() {
+        assertEquals(1, TripleDouble.TripleDouble(666789L, 12345667L));
+    }
+}
