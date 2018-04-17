09a22d77f19aced7cef7a931aca8cfcf98e92fb1
Sebastian De Ro <sebastian.dero@yahoo.de>
2018-04-17 20:17:33 +0200

    final

--- src/com/sebastiandero/kata/BattleShips.java
+++ src/com/sebastiandero/kata/BattleShips.java
@@ -10,20 +10,46 @@ public class BattleShips {
     public static Map<String, Double> damagedOrSunk(final int[][] board, final int[][] attacks) {
         ships = new Ship[3];
         // Your code here...
+
+        fillWithShips(board);
+        simulateAttacks(board, attacks);
+
+        //build return list
         List<Ship> shipsList = Arrays.stream(ships).filter(Objects::nonNull).collect(Collectors.toList());
 
-        double sunk = shipsList.stream().filter(Ship::isSunk).count();
-        double damaged = shipsList.stream().filter(Ship::isTouched).count();
-        double notTouched = shipsList.stream().filter(Ship::notTouched).count();
-        double points = shipsList.stream().mapToDouble(Ship::getScore).sum();
-
         Map<String, Double> returnMap = new HashMap<>();
-        returnMap.put("sunk", sunk);
-        returnMap.put("damaged", damaged);
-        returnMap.put("notTouched", notTouched);
-        returnMap.put("points", points);
+        returnMap.put("sunk", (double) shipsList.stream().filter(Ship::isSunk).count());
+        returnMap.put("damaged", (double) shipsList.stream().filter(ship -> !ship.isSunk()).filter(Ship::isTouched).count());
+        returnMap.put("notTouched", (double) shipsList.stream().filter(ship -> !ship.isTouched()).count());
+        returnMap.put("points", shipsList.stream().mapToDouble(Ship::getScore).sum());
 
         return returnMap;
+    }
+
+    private static void simulateAttacks(int[][] board, int[][] attacks) {
+        for (int[] attack : attacks) {
+            int y = board.length - (attack[1]);
+            int boat = board[y][attack[0] - 1];
+            if (boat != 0) {
+                ships[boat - 1].hitShip();
+            }
+        }
+    }
+
+    private static void fillWithShips(int[][] board) {
+        for (int i = 0; i < 3; i++) {
+            int count = 0;
+            for (int[] boardRow : board) {
+                for (int field : boardRow) {
+                    if (field == i + 1) {
+                        count++;
+                    }
+                }
+            }
+            if (count != 0) {
+                ships[i] = new Ship(count);
+            }
+        }
     }
 
     private static class Ship {
@@ -31,11 +57,11 @@ public class BattleShips {
         int hits;
         int size;
 
-        public Ship(int size) {
+        Ship(int size) {
             this.size = size;
         }
 
-        public void hitShip() {
+        void hitShip() {
             hits++;
         }
 
@@ -43,12 +69,8 @@ public class BattleShips {
             return hits == size;
         }
 
-        private boolean notTouched() {
-            return hits == 0;
-        }
-
         private boolean isTouched() {
-            return !notTouched();
+            return hits != 0;
         }
 
         private double getScore() {
