aa873b56ad91976b58cbed578002d123a1b74e4e
Sebastian De Ro <sebastian.dero@yahoo.de>
2018-04-17 23:22:56 +0200

    iterative

--- src/com/sebastiandero/kata/Boggle.java
+++ src/com/sebastiandero/kata/Boggle.java
@@ -1,106 +1,109 @@
 package com.sebastiandero.kata;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
+import java.util.Arrays;
 
 public class Boggle {
-    private ArrayList<Coordinate> coordinatesHistory;
+
     private char[][] board;
-    private String word;
+    private char[] word;
+    private int maxRepetition;
+
 
     public Boggle(final char[][] board, final String word) {
         this.board = board;
-        this.word = word;
-        coordinatesHistory = new ArrayList<>();
+        this.word = word.toCharArray();
+
+        int[] repetitions = new int[Character.MAX_VALUE];
+
+        for (char[] row : board) {
+            for (char c : row) {
+                repetitions[c]++;
+            }
+        }
+        maxRepetition = Arrays.stream(repetitions).max().orElse(0);
     }
 
     public boolean check() {
-        List<Coordinate> firstLetterCoordinates = findLetter(board, word.charAt(0));
-        for (Coordinate coordinate : firstLetterCoordinates) {
-            if (doNext(coordinate, 1)) {
-                return true;
+        int[][][] tree = new int[word.length][maxRepetition][2];
+        int[] subTreeIterator = new int[word.length + 1];
+        if (word.length != 0 && maxRepetition != 0) {
+            tree[0] = new int[][]{{-1, -1}};
+        }
+        int[] currentCoords;
+
+        for (int i = 0; i < word.length; i++) {
+            if (subTreeIterator[i] >= tree[i].length) {
+                i -= 2;
+                if (i < -1) {
+                    return false;
+                }
+                subTreeIterator[i + 1]++;
+                continue;
+            }
+            currentCoords = tree[i][subTreeIterator[i]];
+
+
+            int[][] possibleCoordinates = getPossibleCoordinates(word[i]);
+            int[][] nextCoordinates = getNextCoordinates(possibleCoordinates, currentCoords, tree, subTreeIterator, i);
+
+            if (nextCoordinates.length == 0) {
+                if (i == 0) {
+                    return false;
+                }
+                i--;
+                subTreeIterator[i + 1]++;
+                continue;
+            }
+            if (i + 1 < tree.length) {
+                tree[i + 1] = nextCoordinates;
+            } else {
+                break;
             }
         }
-        return false;
+        return true;
     }
 
-    private boolean doNext(Coordinate coordinate, int index) {
-        if (coordinatesHistory.contains(coordinate)) {
-            return false;
-        }
-        coordinatesHistory.add(coordinate);
-        if (word.length() <= index) {
-            return true;
-        }
-
-        char[] charArray = word.toCharArray();
-
-        if (index < charArray.length) {
-            List<Coordinate> letterCoordinates = findLetter(board, charArray[index]);
-            List<Coordinate> nextPossible = getOneAway(coordinate, letterCoordinates);
-
-            if (nextPossible.size() == 0) {
-                coordinatesHistory.remove(coordinate);
-                return false;
-            }
-
-            for (Coordinate c : nextPossible) {
-                if (doNext(c, index + 1)) {
-                    return true;
+    private int[][] getNextCoordinates(int[][] possibleCoordinates, int[] currentCoords, int[][][] tree, int[] subTreeIterator, int currentCharacterInWord) {
+        int[][] nextCoords = new int[maxRepetition][2];
+        int iterator = 0;
+        for (int[] pc : possibleCoordinates) {
+            if (currentCoords[0] == -1 && currentCoords[1] == -1) {
+                nextCoords[iterator] = pc;
+                iterator++;
+            } else if (pc[0] <= currentCoords[0] + 1 && currentCoords[0] - 1 <= pc[0]) {
+                if (pc[1] <= currentCoords[1] + 1 && currentCoords[1] - 1 <= pc[1]) {
+                    boolean alreadyUsed = false;
+                    for (int i = 1; i < currentCharacterInWord; i++) {
+                        if (subTreeIterator[i] < tree[i].length) {
+                            int[] treeCords = tree[i][subTreeIterator[i]];
+                            if (treeCords[0] == pc[0] && treeCords[1] == pc[1]) {
+                                alreadyUsed = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (!alreadyUsed) {
+                        nextCoords[iterator] = pc;
+                        iterator++;
+                    }
                 }
             }
         }
-        coordinatesHistory.remove(coordinate);
-        return false;
+        return Arrays.copyOfRange(nextCoords, 0, iterator);
     }
 
-    private List<Coordinate> getOneAway(Coordinate current, List<Coordinate> letterCoordinates) {
-        return letterCoordinates.stream().filter(current::bordering).collect(Collectors.toList());
-    }
-
-    private List<Coordinate> findLetter(char[][] board, char letter) {
-        List<Coordinate> list = new ArrayList<>();
+    private int[][] getPossibleCoordinates(char character) {
+        int[][] ret = new int[maxRepetition][2];
+        int iterator = 0;
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] == letter) {
-                    list.add(new Coordinate(j, i));
+                if (board[i][j] == character) {
+                    ret[iterator][0] = i;
+                    ret[iterator][1] = j;
+                    iterator++;
                 }
             }
         }
-        return list;
-    }
-
-    private class Coordinate {
-        int x;
-        int y;
-
-        Coordinate(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        boolean bordering(Coordinate other) {
-            if (x + 1 >= other.x && other.x >= x - 1) {
-                return y + 1 >= other.y && other.y >= y - 1;
-            }
-            return false;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Coordinate that = (Coordinate) o;
-            return x == that.x &&
-                    y == that.y;
-        }
-
-        @Override
-        public int hashCode() {
-
-            return Objects.hash(x, y);
-        }
+        return Arrays.copyOfRange(ret, 0, iterator);
     }
 }
